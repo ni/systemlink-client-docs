c2fa88120c8fc7eb00c4d16d3e2b9c94ed788195
Matthew Vaterlaus <matthew.vaterlaus@ni.com>
2020-04-14 12:28:00 -0500

    Move the functionality into main.

--- examples/file/stream/Stream.cs
+++ examples/file/stream/Stream.cs
@@ -7,8 +7,8 @@ using NationalInstruments.SystemLink.Clients.File;
 namespace NationalInstruments.SystemLink.Clients.Examples.File
 {
     /// <summary>
-    /// Example for the SystemLink Test Monitor API that creates a test result
-    /// and associated steps simulating a current and voltage sweep.
+    /// Example for the SystemLink File API that uploads a file to the
+    /// SystemLink server using an in-memory stream.
     /// </summary>
     class Stream
     {
@@ -20,22 +20,11 @@ namespace NationalInstruments.SystemLink.Clients.Examples.File
              */
             var configuration = ExampleConfiguration.Obtain(args);
 
-            /*
-             * Create the TestDataManager for communicating with the server.
-             */
+            // Use the FileUploader for communicating with the server.
             var fileUploader = new FileUploader(configuration);
 
-            // Upload some sample data as the file contents.
-            var fileId = UploadFileUsingStream(
-                configuration,
-                "stream.txt",
-                Encoding.UTF8.GetBytes("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-        }
-
-        public static string UploadFileUsingStream(Core.IHttpConfiguration configuration, string fileName, byte[] fileContents)
-        {
-            // Use the FileUploader from the SystemLink File client
-            var fileUploader = new FileUploader(configuration);
+            var fileName = "stream.txt";
+            var fileContents = Encoding.UTF8.GetBytes("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
             // Use in-memory data for the file upload.
             using (var memoryStream = new MemoryStream())
@@ -48,7 +37,6 @@ namespace NationalInstruments.SystemLink.Clients.Examples.File
 
                 // Upload the file to the SystemLink server and get the uploaded file's Id.
                 var fileId = fileUploader.UploadFile(memoryStream, fileName);
-                return fileId;
             }
         }
     }
